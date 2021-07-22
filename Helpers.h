2fb7e02178b7accec116fcf4925e53d3549182cc
Tyler Rolfe <tylermrolfe@gmail.com>
2021-07-22 16:22:02 -0400

    Added Hibbard sequence function

--- Helpers.h
+++ Helpers.h
@@ -21,13 +21,31 @@ bool isUndefined(T val) {
     }
 }
 
+/// Genereates the Sedgewick, 1982 Sequence for gaps with worst case time complexity of O(n^4/3)
+/// \param size - The size of the array you are attempting to shell sort
+/// \return
 vector<int> generateSedgewickSequence(int size) {
     vector<int> sequence;
     int val = 1;
     int index = 1;
     sequence.push_back(val);
     while (val < size) {
-        val = ((pow(4, index) + (3*pow(2, index-1) + 1)));
+        val = int(((pow(4, index) + (3*pow(2, index-1) + 1))));
+        sequence.push_back(val);
+        index++;
+    }
+    return sequence;
+}
+
+/// Genereates the Hibbard Sequence for gaps with worst case time complexity of O(n^3/2)
+/// \param size - The size of the array you are attempting to shell sort
+/// \return
+vector<int> generateHibbardSequence(int size) {
+    vector<int> sequence;
+    int val = 0;
+    int index = 1;
+    while (val < size) {
+        val = int((pow(2, index) - 1));
         sequence.push_back(val);
         index++;
     }
